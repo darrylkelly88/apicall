086b178e1830087d24cad44f9d12951ad69db660
darrylkelly88 <88737523+darrylkelly88@users.noreply.github.com>
2023-03-21 14:38:17 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+import json
+from requests.structures import CaseInsensitiveDict
+
+def apicall():
+
+                url = "https://aap.redhat.tdbsc.co.uk/api/v2/workflow_job_templates/13/launch"
+
+                PAT_token = "Bearer nsoNIspjEuAo8dwt2ZPYJqBXomADcC"
+
+                headers = CaseInsensitiveDict()
+                headers["Authorization"] = PAT_token
+                headers["Content-Type"] = "application/json"
+                data = """
+
+                {
+
+                    "ask_limit_on_launch": false,
+
+                    "ask_scm_branch_on_launch": false
+
+                }
+
+                """
+
+                resp = requests.post(url, headers=headers, data=data)
+                data = resp.json()
+                print(data)
+                print(resp.status_code)
+
+
+apicall()
